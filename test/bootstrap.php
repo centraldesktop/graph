ac684a055fd17b876e0b0d975fc28ed69e1f20c7
Trey Hyde <thyde@centraldesktop.com>
2013-04-18 15:21:47 -0700

    Fix bootsrap path again

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-$autoload_path =  __DIR__ . "/../../../../vendor/autoload.php";
+$autoload_path =  __DIR__ . "/../vendor/autoload.php";
 $loader = require($autoload_path);
 
 $loader->add('CentralDesktop\Graph\Test', __DIR__);
